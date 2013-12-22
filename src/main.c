9197990662050125dcba50dcac02273b43bd77eb
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-12-22 15:59:38 +0400

    initial
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "version.h"
+#include <glib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+gint main ()
+{
+  guint N = 0; // Count cities
+  gint i, j;
+  g_printf ("Input cities count..\n");
+  scanf ("%u", &N);
+  guint **D = g_new0 (guint *, N + 1);
+  for (i = 0; i < N; i++)
+    D[i] = g_new (guint, N + 1);
+  g_printf ("Input cities destination matrix..\n");
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++) {
+      if (i == j) continue;
+      g_printf ("Input D[%u][%u]: ", i, j);
+      scanf("%u", &D[i][j]);
+    }
+  g_free (D);
+  return 0;
+}
